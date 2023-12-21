8f3043f5ac585acc16e1717ed944b0da1a24057f
Gaspard Lonchampt <gaspard.lonchampt@beta.gouv.fr>
2023-12-21 23:53:40 +0100

    feat(competence): add sass

--- js/main.js
+++ js/main.js
@@ -2,45 +2,44 @@ AOS.init({ once: true });
 
 $(window).scroll(function () {
   $("#show").toggleClass(
-    "is-fixed-top is-transparent",
-    $(this).scrollTop() > 600
+    "is-fixed-top",
+    $(this).scrollTop() > 100
   );
 });
 
-$(document).ready(function() {
+$(document).ready(function () {
 
   // Check for click events on the navbar burger icon
-  $(".navbar-burger").click(function() {
+  $(".navbar-burger").click(function () {
 
-      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-      $(".navbar-burger").toggleClass("is-active");
-      $(".navbar-menu").toggleClass("is-active");
+    // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+    $(".navbar-burger").toggleClass("is-active");
+    $(".navbar-menu").toggleClass("is-active");
 
   });
 
-    // Check for click events on the navbar burger icon
-    $(".has-dropdown").click(function() {
+  // Check for click events on the navbar burger icon
+  $(".has-dropdown").click(function () {
 
-      // Toggle the "is-active" class on the "navbar-menu"
-      $(".has-dropdown").toggleClass("is-active");
+    // Toggle the "is-active" class on the "navbar-menu"
+    $(".has-dropdown").toggleClass("is-active");
 
   });
 });
 
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener('click', function (e) {
-      e.preventDefault();
+    e.preventDefault();
 
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
-          behavior: 'smooth'
-      });
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
   });
 });
 
-$('a[href="#top"]').on('click', function(e) {
+$('a[href="#top"]').on('click', function (e) {
   e.preventDefault();
   $('body, html').scrollTop(0, {
     behavior: 'smooth'
+  });
 });
-});
-
